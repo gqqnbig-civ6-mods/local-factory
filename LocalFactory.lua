4d912ffd6b29c35cfad31f89036ddb6585cccc96
QQ喵 <gqqnb2005@gmail.com>
2020-04-16 11:09:24 -0700

    检测是否有两座发电厂或本地发电厂

--- LocalFactory.lua
+++ LocalFactory.lua
@@ -14,22 +14,72 @@ end
 ]]
 
 
+--- OnBuildingConstructed desc
+---@param cityType number 如city center, campus, harbor等
+function OnBuildingConstructed(playerID, cityType, buildingID, plotID, bOriginalConstruction)
+    -- https://www.lua.org/manual/5.2/manual.html#6.1
+    -- print可以接受任意数量的参数
+    -- print("OnBuildingConstructed", playerID, cityType, buildingID, plotID, bOriginalConstruction)
+	local building = GameInfo.Buildings[buildingID]
+
+
+
+
+	local player=Players[playerID]
+    local techs = player:GetTechs()
+
+    print(techs)
+
+    if building ~= nil and (building.Name == 'LOC_BUILDING_POWER_PLANT_NAME' or building.Name == 'LOC_BUILDING_LOCAL_POWER_PLANT_NAME') then
+        print('发电厂造好了')
+
+        if player.PowerPlantCount == nil then
+			player.PowerPlantCount=0
+			local localPowerPlantInfo = GameInfo.Buildings['BUILDING_LOCAL_POWER_PLANT']
+			local regionalPowerPlantInfo = GameInfo.Buildings['BUILDING_POWER_PLANT']
+			print(localPowerPlantInfo,localPowerPlantInfo.Index)
+
+			for _, city in player:GetCities():Members() do
+				local cityBuildings= city:GetBuildings()
+				if cityBuildings:HasBuilding(localPowerPlantInfo.Index) then
+					player.PowerPlantCount=player.PowerPlantCount+1
+					print(city:GetName(),'has local')
+				elseif cityBuildings:HasBuilding(regionalPowerPlantInfo.Index) then
+					player.PowerPlantCount=player.PowerPlantCount+1
+					print(city:GetName(),'has regional')
+				end
+			end
+        else
+            player.PowerPlantCount = player.PowerPlantCount + 1
+        end
+
+		print(player.PowerPlantCount)
+		if player.PowerPlantCount>=2 then
+			print('有两座发电厂或本地发电厂了')
+		end
+    end
+end
+
+--[[
+--- OnBuildingAddedToMap desc
 ---@param plotX number
 ---@param plotY number
 ---@param buildingType number
 ---@param playerType number
 ---@param pctComplete number
 ---@param bPillaged number
-
 function OnBuildingAddedToMap(plotX, plotY, buildingType, playerType, pctComplete, bPillaged)
-	print(plotX);
-	print(plotY);
-	print(buildingType);
-	print(playerType);
-	print(pctComplete);
-	print(bPillaged);
+	print('OnBuildingAddedToMap', plotX, plotY, buildingType, playerType, pctComplete, bPillaged);
 end
+]]
 
-print("LocalFactory_OnBuildingAddedToMap");
+print("LocalFactory_OnBuildingAddedToMap")
 
-Events.BuildingAddedToMap.Add(OnBuildingAddedToMap);
+--Events.BuildingAddedToMap.Add(OnBuildingAddedToMap);
+GameEvents.BuildingConstructed.Add(OnBuildingConstructed)
+
+--[[
+
+ LocalFactory: OnBuildingConstructed	0	655366	20	3457	true
+ LocalFactory: OnBuildingAddedToMap		13	41	20	0	655366	0
+]]
